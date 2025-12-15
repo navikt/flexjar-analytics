7375550050c5fdea2e07519e5d5fce06da7d25cb
Audun Sørheim <audun.sorheim@nav.no>
2025-12-15 15:31:11 +0100

    Inline createRouter

--- app/router.tsx
+++ app/router.tsx
@@ -2,12 +2,10 @@ import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
 export function getRouter() {
-  const router = createRouter({
+  return createRouter({
     routeTree,
     scrollRestoration: true,
   });
-
-  return router;
 }
 
 declare module "@tanstack/react-router" {
